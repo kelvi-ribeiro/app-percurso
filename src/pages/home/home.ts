5865ad2a26d79e3edb2e353aab4526b129eef085
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-06-10 15:48:05 -0300

    Formulario de definicao de configuracao do jogo funcionando

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,82 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormBuilder, Validators,FormGroup } from '@angular/forms';
+import { ConfiguracaoJogoService } from '../../services/domain/configuracao-jogo.service';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  cores;
+  quantidadePerguntaAvatar;
+  numeroCoresAvatarInput = [];
+  quantidadeMensagem;
+  mensagemInput = [];
 
-  constructor(public navCtrl: NavController) {
-
+  formGroup: FormGroup;
+  constructor(
+    public navCtrl: NavController,
+    public formBuilder: FormBuilder,
+    public configuracaoJogoService:ConfiguracaoJogoService
+  ) {
+    this.cores = [
+      {
+        valor:0,
+        cor:'Azul'
+      },
+      {
+        valor:1,
+        cor:'Amarelo'
+      },
+      {
+        valor:2,
+        cor:'Preto'
+      }
+    ];
+    this.quantidadePerguntaAvatar = [
+      {
+      }
+    ]
+    this.quantidadeMensagem = [
+      {
+      }
+    ]
+    this.formGroup = this.formBuilder.group({
+      coresTabuleiro: [null, [Validators.required]],
+      coresAvatar: [[], [Validators.required]],
+      mensagensUltimaCasa: [[], [Validators.required]]
+    });
   }
 
-}
+  adicionarCorAvatar(){
+    this.quantidadePerguntaAvatar.push({});
+  }
+  inserirCor(e){
+    this.numeroCoresAvatarInput.push(e)
+    console.log(this.numeroCoresAvatarInput)
+  }
+
+  adicionarMensagem(){
+    this.quantidadeMensagem.push({});
+  }
+  inserirMensagem(e){
+    this.mensagemInput.push(e)
+    console.log('inserirMensagem',this.mensagemInput)
+  }
+
+  enviarConfiguracao(){
+    this.formGroup.controls.coresAvatar.setValue(this.numeroCoresAvatarInput);
+    this.formGroup.controls.mensagensUltimaCasa.setValue(this.mensagemInput);
+
+    this.configuracaoJogoService.cadastrarConfiguracoesGerais(this.formGroup.value)
+    .subscribe(response=>{
+      console.log('response',response)
+    },
+  error =>{});
+  }
+  }
+
+
+
+
---
+++ src/services/domain/configuracao-jogo.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs/Rx";
+import { API_CONFIG } from "../../config/api.config";
+@Injectable()
+export class ConfiguracaoJogoService {
+  constructor(public http: HttpClient) {
+  }
+
+
+  cadastrarConfiguracoesGerais(obj) {
+    console.log('cadastrarConfiguracoesGerais',obj);
+
+    return this.http.post(`${API_CONFIG.baseUrl}/programa/config`, obj, {
+      observe: "response",
+      responseType: "text"
+    });
+  }
+}
