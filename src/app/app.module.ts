a0ad6626ba8fabe18b1aeec515f401d21fdc26db
Kelvi Martins Ribeiro <kelvi.ribeiro@unigranrio.br>
2018-06-12 10:24:13 -0300

    Removendo paginas desnecessarias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -16,8 +14,6 @@ import { ConfiguracaoJogoService } from '../services/domain/configuracao-jogo.se
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
@@ -30,8 +26,6 @@ import { ConfiguracaoJogoService } from '../services/domain/configuracao-jogo.se
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
     TabsPage
   ],
